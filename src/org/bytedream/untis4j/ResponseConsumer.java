325b04f1e0bfe4a69e87cbdfd85fc779a983eaa0
ByteDream <bytedream@protonmail.com>
2020-09-10 22:21:23 +0200

    Reformat file and add javadoc

--- src/org/bytedream/untis4j/ResponseConsumer.java
+++ src/org/bytedream/untis4j/ResponseConsumer.java
@@ -1,8 +1,15 @@
 package org.bytedream.untis4j;
 
 import org.bytedream.untis4j.responseObjects.baseObjects.BaseResponse;
+
 import java.io.IOException;
 
+/**
+ * Consumer for request responses
+ *
+ * @version 1.0
+ * @since 1.1
+ */
 public interface ResponseConsumer<T extends BaseResponse> {
 
     T getResponse(Response response) throws IOException;
