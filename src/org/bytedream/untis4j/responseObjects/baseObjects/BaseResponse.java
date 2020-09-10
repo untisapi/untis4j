ca5563cdc58083c3398163ceae0da49afd14f5f5
ByteDream <bytedream@protonmail.com>
2020-09-10 13:44:29 +0200

    Add object to identify which classes are part of the ResponseLists / ResponseObjects

---
+++ src/org/bytedream/untis4j/responseObjects/baseObjects/BaseResponse.java
@@ -0,0 +1,10 @@
+package org.bytedream.untis4j.responseObjects.baseObjects;
+
+/**
+ * The absolute base interface for all base response lists / objects
+ *
+ * @version 1.0
+ * @since 1.1
+ */
+public interface BaseResponse {
+}
